a97fc7e087fc41aa7dc83ef2b05d98f3332e114c
Daniel Lee <daniellee9707@gmail.com>
2019-09-25 15:00:25 -0700

    with mapkit

--- CarPal/ViewControllers/HomeViewController.swift
+++ CarPal/ViewControllers/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class HomeViewController: UIViewController{
 
@@ -15,8 +16,14 @@ class HomeViewController: UIViewController{
     
     @IBOutlet weak var mapView: MKMapView!
     
+    // location manager constant
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // check location service
+        checkLocationServices()
 
         // Do any additional setup after loading the view.
         signOutButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
@@ -33,5 +40,49 @@ class HomeViewController: UIViewController{
         view.window?.rootViewController = firstController
         view.window?.makeKeyAndVisible()
     }
+    
+    // setting up location manager
+    private func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled(){
+            setUpLocationManager()
+            checkLocationAutorization()
+        }else {
+            // show message that notify the user to turn on the location service
+            
+        }
+    }
+    
+    // check location authorization
+    private func checkLocationAutorization() {
+        switch CLLocationManager.authorizationStatus(){
+        case .authorizedWhenInUse:
+            break
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
 
+}
+
+extension HomeViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+    }
+    
+    
 }
