02a0c621caf17f01aafdd8cd454409601673a6e8
Daniel Lee <daniellee9707@gmail.com>
2019-11-22 00:59:07 -0800

    updated

--- CarPal/ViewControllers/HomeViewController.swift
+++ CarPal/ViewControllers/HomeViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 import MapKit
 import CoreLocation
+import Firebase
 
 class HomeViewController: UIViewController{
 
     @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var matchingButton: UIButton!
+    @IBOutlet weak var addressLabel: UILabel!
     
     @IBOutlet weak var mapView: MKMapView!
     
     // location manager constant
     let locationManager = CLLocationManager()
     let regionInMeter: Double = 10000
+    var previousLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +31,18 @@ class HomeViewController: UIViewController{
         checkLocationServices()
 
         // Do any additional setup after loading the view.
+        setUpElement()
         signOutButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         signOutButton.layer.cornerRadius = 15.0
         signOutButton.tintColor = UIColor.white
+    }
+    
+    @IBAction func matchButtonTapped(_ sender: Any) {
+        
+    }
+    
+    private func getAsdress(){
+        
     }
     
     @IBAction func signOutTapped(_ sender: Any) {
@@ -69,10 +82,7 @@ class HomeViewController: UIViewController{
     private func checkLocationAutorization() {
         switch CLLocationManager.authorizationStatus(){
         case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
+            startTrackingUserLocation()
         case .denied:
             break
         case .notDetermined:
@@ -82,6 +92,24 @@ class HomeViewController: UIViewController{
         case .authorizedAlways:
             break
         }
+    }
+    
+    func startTrackingUserLocation(){
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
+    }
+    
+    private func setUpElement(){
+        Utilities.styleFilledButton(matchingButton)
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
     }
 
 }
@@ -98,4 +126,34 @@ extension HomeViewController: CLLocationManagerDelegate{
         checkLocationAutorization()
     }
     
+}
+
+extension HomeViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        
+        guard let previousLocation = self.previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in guard let self = self else { return }
+            
+            if let _ = error {
+                return
+            }
+            
+            guard let placemark = placemarks?.first else {
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+        }
+    }
 }
